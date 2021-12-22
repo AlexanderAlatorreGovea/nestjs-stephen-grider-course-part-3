693004aa14a1c810adfe82b05e31516e0ecb23a5
Alexander <Alexander_alatorre1993@hotmail.com>
2021-12-21 17:51:39 -0800

    finished sign in and out service

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,14 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+
 import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -12,8 +21,28 @@ export class AuthService {
       throw new BadRequestException('email in use.');
     }
 
-    
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+    const user = await this.userService.create(email, result);
+
+    return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    const [user] = await this.userService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+    }
+
+    return user;
+  }
 }
